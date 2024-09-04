431e4441defbd13ed13ff98834b0566ef470bde5
Дмитрий <dimats2350@gmail.com>
2024-09-05 00:46:26 +0200

    drag(not working)

--- ego/main/static/js/mapMaker.js
+++ ego/main/static/js/mapMaker.js
@@ -4,9 +4,9 @@ const amountOfColumns = 8;
 function makeRow(kol) {
     let teil_ = document.createElement('div');
 
-    let imgRow = `<img src='static/img/mapPieces/p0${kol}.jpg' />`;
+    let imgRow = `<img class='TeilDesMap' draggable="true" src='static/img/mapPieces/p0${kol}.jpg' />`;
     for (let i = 1; i < amountOfColumns; i++) {
-        imgRow += `<img class='TeilDesMap' src='static/img/mapPieces/p${i}${kol}.jpg' />`;
+        imgRow += `<img class='TeilDesMap' draggable="true" src='static/img/mapPieces/p${i}${kol}.jpg' />`;
     }
     teil_.classList.add("imgRow");
     teil_.innerHTML = imgRow;
@@ -22,39 +22,39 @@ mapObject_.innerHTML = festung;
 document.getElementById('worldMap').appendChild(mapObject_);
 
 document.addEventListener('DOMContentLoaded', function () {
-    const festung1 = document.getElementById('festung_1')
+    const festung1 = document.getElementById('festung_1');
     const worldMap = document.getElementById('worldMap');
-    const mainArea = document.querySelector('#mainArea');
-    let isDragging = false;
-    let startX, startY, initialLeft, initialTop;
+    let coordinateX = 0;
+    let coordinateY = 0;
+    let dragging;
 
-    mainArea.addEventListener('mousedown', function (e) {
-        startX = e.clientX;
-        startY = e.clientY;
-        initialLeft = worldMap.offsetLeft;
-        initialTop = worldMap.offsetTop;
-        isDragging = true;
-        document.addEventListener('mouseup', onMouseUp);
-        document.addEventListener('mousemove', onMouseMove);
-    });
+    worldMap.addEventListener("dragstart", function (e ) {
+        let shiftX = e.clientX;
+        let shiftY = e.clientY;
 
-    festung1.addEventListener("dblclick", function (e){
-        alert();
-    });
-
-    function onMouseMove(e) {
-        if (isDragging) {
-            const deltaX = e.clientX - startX;
-            const deltaY = e.clientY - startY;
-            worldMap.style.left = initialLeft + deltaX + 'px';
-            worldMap.style.top = initialTop + deltaY + 'px';
+        // Функция для перемещения элемента
+        function moveAt(pageX, pageY) {
+            worldMap.style.left = coordinateX + pageX - shiftX + 'px';
+            worldMap.style.top = coordinateY + pageY - shiftY + 'px';
+            console.log(e.clientX, shiftX, e.clientY, shiftY);
         }
-    }
-    function onMouseUp() {
-        isDragging = false;
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
-    }
+
+        // Обработчик движения мыши
+        function onDrag(e) {
+            moveAt(e.clientX, e.clientY);
+        }
+
+        function ondragend(e){
+            coordinateX = e.clientX - shiftX;
+            coordinateY = e.clientY - shiftY;
+        }
+
+        document.addEventListener('drag', onDrag);
+        document.addEventListener('dragend', ondragend);
+
+    });
+
+    festung1.addEventListener("dblclick", function () {
+        alert('Double-clicked!');
+    });
 });
-
-
