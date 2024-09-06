f5279595095474139ceaac1ddf86255217794a38
Дмитрий <dimats2350@gmail.com>
2024-09-06 20:49:56 +0200

    users app

--- ego/main/views.py
+++ ego/main/views.py
@@ -6,73 +6,16 @@ from django.views import generic
 from django.template import loader
 from django.http import HttpResponse
 from django.utils import timezone
-from .models import Choice, Question
-
-
-# def print_start_html(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("main/login_page.html")
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
-def test(request):
-    return render(request, 'main/test.html')
 
 
 def world(request):
     return render(request, 'main/world_page.html')
 
 
+def test(request):
+    return render(request, 'main/test.html')
+
+
 def login(request):
     return render(request, 'main/login_page.html')
 
-
-class IndexView(generic.ListView):
-    template_name = "main/login_page.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "main/detail.html"
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "main/results.html"
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "main/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("main:results", args=(question.id,)))
