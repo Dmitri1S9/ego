ada54c518d8ea961ce6d4f1d3651af8aebdd9cd0
Дмитрий <dimats2350@gmail.com>
2024-09-06 23:00:03 +0200

    users app rework

--- ego/ego/urls.py
+++ ego/ego/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wolrd/', include('main.urls')),
+    path('main/', include('main.urls')),
     path('', include('users.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
